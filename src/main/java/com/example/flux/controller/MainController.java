dc163514d27cc9e7f5b6825969cbe6ceb7caf718
laisk <laisk1@hotmail.com>
2021-04-26 22:57:16 +0300

    @GetMapping @PostMapping

--- src/main/java/com/example/flux/controller/MainController.java
+++ src/main/java/com/example/flux/controller/MainController.java
@@ -1,28 +1,32 @@
 package com.example.flux.controller;
 
 import com.example.flux.dom.Message;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.flux.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/controller")
 public class MainController {
+    private final MessageService messageService;
+
+    @Autowired //deprecated
+    public MainController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
     @GetMapping
     Flux<Message> list(
             @RequestParam(defaultValue = "0") Long start
             , @RequestParam(defaultValue ="5") Long count
     ){
-        return Flux
-                .just(
-                        "First",
-                        "Second",
-                        "One more"
-                )
-                .skip(start)
-                .take(count)
-                .map(Message::new);
+        return messageService.list();
+    }
+
+    @PostMapping
+    Mono<Message> add(Message message){
+        return messageService.addOneMessage(message);
     }
 }
