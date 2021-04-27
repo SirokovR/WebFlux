3fe758330879b720b1b3d44bd692af45e7c568bd
laisk <laisk1@hotmail.com>
2021-04-27 16:09:38 +0300

    findByUsername

---
+++ src/main/java/com/example/flux/repo/UserRepo.java
@@ -0,0 +1,9 @@
+package com.example.flux.repo;
+
+import com.example.flux.dom.User;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
+
+public interface UserRepo extends ReactiveCrudRepository<User, Long> {
+    Mono<User> findByUsername(String name);
+}
