0e41a51c40a58ef766a6396a713488910294ece8
laisk <laisk1@hotmail.com>
2021-04-28 14:39:25 +0300

    privat --> final

--- src/main/java/com/example/flux/client/GreetingWebClient.java
+++ src/main/java/com/example/flux/client/GreetingWebClient.java
@@ -7,9 +7,9 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 public class GreetingWebClient {
-  private WebClient client = WebClient.create("http://localhost:8080");
+  private final WebClient client = WebClient.create("http://localhost:8080");
 
-  private Mono<ClientResponse> result = client.get()
+  private final Mono<ClientResponse> result = client.get()
       .uri("/hello")
 /*      .accept(MediaType.TEXT_PLAIN)
       .exchange();*/
