a6e84d305cd79bdd834b581ecea8bc6e30764448
laisk <laisk1@hotmail.com>
2021-04-29 01:41:13 +0300

    validateToken()

--- src/main/java/com/example/flux/config/JwtUtil.java
+++ src/main/java/com/example/flux/config/JwtUtil.java
@@ -1,23 +1,57 @@
 package com.example.flux.config;
 
+import com.example.flux.dom.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 
 @Component
 
 public class JwtUtil {
     @Value("${jwt.secret}")
     private String secret;
+    @Value("${jwt.expiration}")
+    private String expirationTime;
     public String extractUserName(String authToken) {
+        return getClaimsFromThoken(authToken)
+                .getSubject();
+    }
+
+    public Claims getClaimsFromThoken(String authToken) {
         String key = Base64.getEncoder().encodeToString(secret.getBytes());
         return Jwts.parserBuilder()
                 .setSigningKey(key)
                 .build()
                 .parseClaimsJws(authToken)
-                .getBody()
-                .getSubject();
+                .getBody();
     }
+
+    public boolean validateToken(String authToken) {
+        return getClaimsFromThoken(authToken)
+                .getExpiration()
+                .before(new Date());
+    }
+
+    public String generateToken(User user) {
+        HashMap<String, Object> claims = new HashMap<>();
+        claims.put("role", List.of(user.getRole()));
+        long expirationSeconds = Long.parseLong(expirationTime);
+        Date creationDate = new Date();
+        Date expirationDate = new Date(creationDate.getTime() + expirationSeconds * 1000);
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(user.getUsername())
+                .setIssuedAt(creationDate)
+                .setExpiration(expirationDate)
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .compact();
+    }
+
 }
